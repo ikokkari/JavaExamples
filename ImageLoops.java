07cf8e85a0390beb098cdff20950874dc7078d8b
Ilkka Kokkarinen <ilkka.kokkarinen@gmail.com>
2020-12-01 09:25:49 -0500

    Add files via upload

--- ImageLoops.java
+++ ImageLoops.java
@@ -10,6 +10,11 @@ public class ImageLoops {
     // Objects of class Color represent individual colours. However, for low-level
     // drawing, the colours are represented and encoded as four-byte integers.
     private static final int WHITE = Color.WHITE.getRGB();
+    
+    // Define the size and padding as named constants.
+    private static final int SIZE = 400;
+    private static final int PAD = 40;
+    private static final int BOX = 25;
 
     // Render the pixels (x, y) that satisfy the predicate given as parameter.
     public static BufferedImage computeImage(int w, int h, BiPredicate<Integer, Integer> pred) {
@@ -26,36 +31,36 @@ public class ImageLoops {
 
     // A main method to display the images inside a panel inside a JFrame window. 
     public static void main(String[] args) {
-        JFrame f = new JFrame("Some images made with lambdas");
+        JFrame f = new JFrame("Some images made with lambda expressions");
         f.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         f.setLayout(new FlowLayout());
 
         class ImagePanel extends JPanel {
             private BufferedImage[] imgs = new BufferedImage[6];
             public ImagePanel() {
-                this.setPreferredSize(new Dimension(680, 460));
+                this.setPreferredSize(new Dimension(3*SIZE + 4*PAD, 2*SIZE + 3*PAD));
                 this.setBackground(new Color(30, 30, 60));
-                imgs[0] = computeImage(200, 200,
-                    (x, y) -> (x / 20) % 2 != (y / 20) % 2
+                imgs[0] = computeImage(SIZE, SIZE,
+                    (x, y) -> (x / BOX) % 2 != (y / BOX) % 2
                 );
-                imgs[1] = computeImage(200, 200,
-                    (x, y) -> (x + y) / 20 % 2 == 0
+                imgs[1] = computeImage(SIZE, SIZE,
+                    (x, y) -> (3*x + 2*y) / (BOX * 3) % 2 == 0
                 );
-                imgs[2] = computeImage(200, 200,
-                    (x, y) -> (x / 15) % 2 == 0 && (y / 15) % 2 == 0
+                imgs[2] = computeImage(SIZE, SIZE,
+                    (x, y) -> (x / BOX) % 2 == 0 && ((y + 3*x/BOX) / BOX) % 2 == 0
                 );
-                imgs[3] = computeImage(200, 200, (x, y) ->
-                  (x + y) / 20 % 2 == 0 &&
-                  (x >= y ? (x - y) / 20 % 2 == 0 : (y - x) / 20 % 2 == 1)
+                imgs[3] = computeImage(SIZE, SIZE, (x, y) ->
+                  (x + y) / BOX % 2 == 0 &&
+                  (x >= y ? (x - y) / BOX % 2 == 0 : (y - x) / BOX % 2 == 1)
                 );
-                imgs[4] = computeImage(200, 200, (x, y) -> {
-                    int xd = Math.abs(100 - x) / 15;
-                    int yd = Math.abs(100 - y) / 15;
+                imgs[4] = computeImage(SIZE, SIZE, (x, y) -> {
+                    int xd = Math.abs(SIZE/2 - 1 - x) / BOX;
+                    int yd = Math.abs(SIZE/2 - 1 - y) / BOX;
                     int d = xd > yd? xd: yd; 
                     return (d % 2) == 0;
                 });
-                imgs[5] = computeImage(200, 200, (x, y) -> {
-                    int c = x*x-2*(x|y)+y*y;
+                imgs[5] = computeImage(SIZE, SIZE, (x, y) -> {
+                    int c = x*x-2*(x|y)+y*y + (x^y);
                     c = c % 256;
                     return c < -127 || c > 127;
                 });
@@ -67,7 +72,7 @@ public class ImageLoops {
                     if(imgs[i] != null) {
                         int row = i / 3;
                         int col = i % 3;
-                        g.drawImage(imgs[i], 20 + 220 * col, 20 + 220 * row, this);
+                        g.drawImage(imgs[i], PAD + (PAD + SIZE) * col, PAD + (PAD + SIZE) * row, this);
                     }
                 }
             }
