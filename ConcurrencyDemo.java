dbcca02d2013d5b39874a373340940d1ce2ccdea
Ilkka Kokkarinen <ilkka.kokkarinen@gmail.com>
2022-03-28 22:15:06 -0400

    Add files via upload

--- ConcurrencyDemo.java
+++ ConcurrencyDemo.java
@@ -5,21 +5,21 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 public class ConcurrencyDemo {
 
-    // An inner class for a Runnable task that we use in the next examples. The task
-    // simply first waits a random time and then terminates.
+    // An inner class for a Runnable task used in this example.
       
     private static class SimpleWaiter implements Runnable {
         private static final AtomicInteger TICKET_MACHINE = new AtomicInteger(0);
         private final int id;
         public SimpleWaiter() {
-            this.id = TICKET_MACHINE.incrementAndGet();
+            this.id = TICKET_MACHINE.incrementAndGet(); // Guaranteed atomic
         }
         @Override public void run() {
             System.out.println("Starting waiter #" + id + ".");
             try {
-                // Sleep between 2 and 4 seconds
+                // Make this thread sleep for a random time between 2 and 4 seconds.
                 Thread.sleep(ThreadLocalRandom.current().nextInt(2000) + 2000);
             }
+            // Not allowed to let exceptions fly out, we are forced use this classic antipattern.
             catch(InterruptedException ignored) { }
             System.out.println("Finishing waiter #" + id + ".");
         }
@@ -30,7 +30,8 @@ public class ConcurrencyDemo {
     public static void simpleThreadDemo(int n) {
         for(int i = 0; i < n; i++) {
             Thread t = new Thread(new SimpleWaiter());
-            // Some settings could be made here before starting the thread.
+            // Some settings could be changed here before starting the thread.
+            // We don't, but someone could.
             t.start();
         }
         System.out.println("All waiters created, returning from method.");
@@ -38,11 +39,11 @@ public class ConcurrencyDemo {
 
     // The previous example implemented using an ExecutorService instead of explicitly
     // creating threads. See what happens if you set this up with different argument.
-    private static final ExecutorService es = Executors.newFixedThreadPool(3);
+    private static final ExecutorService taskHandler = Executors.newFixedThreadPool(3);
     
     public static void simpleExecutorDemo(int n) { // Try n = 2, 5, 10.
         for(int i = 0; i < n; i++) {
-            es.submit(new SimpleWaiter());
+            taskHandler.submit(new SimpleWaiter());
         }
         System.out.println("All waiters submitted, returning from method.");
     }
@@ -50,6 +51,9 @@ public class ConcurrencyDemo {
     public static void main(String[] args) {
         simpleThreadDemo(20);
         // simpleExecutorDemo(20);
-    }
 
+        // Make sure that the ExecutorService that we use here releases its threads
+        // once all tasks submitted to it have been completed.
+        taskHandler.shutdown();
+    }
 }
