04f951a83e945a6ca4ae70098db4345f7da567ce
Ilkka Kokkarinen <ilkka.kokkarinen@gmail.com>
2022-09-27 20:58:52 -0400

    Add files via upload

--- ParameterDemo.java
+++ ParameterDemo.java
@@ -6,14 +6,14 @@ public class ParameterDemo {
     
     private static void fooPrimitive(int x) {
         x++;
-        System.out.println("Inside method foo, x is now " + x + "."); // 2
+        System.out.println("Inside method foo, x is now " + x + "."); // 6
     }
     
     public static void demoPrimitive() {
         int a = 5;
-        System.out.println("To start, a is " + a + "."); // 1
+        System.out.println("To start, a is " + a + "."); // 5
         fooPrimitive(a);
-        System.out.println("After call, a is " + a + "."); // 3
+        System.out.println("After call, a is " + a + "."); // 5
     }
     
     private static void fooArray(int[] x) {
@@ -24,9 +24,9 @@ public class ParameterDemo {
     public static void demoArray() {
         int[] a = new int[100];
         a[42] = 5;
-        System.out.println("To start, a[42] is " + a[42] + "."); // 1
+        System.out.println("To start, a[42] is " + a[42] + "."); // 5
         fooArray(a);
-        System.out.println("After call, a[42] is " + a[42] + "."); // 3
+        System.out.println("After call, a[42] is " + a[42] + "."); // 6
     }
 
     public static void main(String[] args) {
