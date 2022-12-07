1dfab1bee763cc29b6e96d4896887348b12f014d
Ilkka Kokkarinen <ilkka.kokkarinen@gmail.com>
2022-12-06 23:23:39 -0500

    Add files via upload

--- MapReduceDemo.java
+++ MapReduceDemo.java
@@ -15,7 +15,7 @@ public class MapReduceDemo {
     // all that anonymous subclass boilerplate rigmarole automatically.
     // This predicate is stateless, so it can be used in a parallel
     // stream of integers as well as sequential stream.
-    private static final IntPredicate primeTester = n -> {
+    private static final IntPredicate PRIME_TESTER = n -> {
         if(n < 2) { return false; }
         if(n == 2) { return true; }
         if(n % 2 == 0) { return false; }
@@ -32,19 +32,19 @@ public class MapReduceDemo {
         System.out.println("The sum of asked squares is " + 
             new Random(12345).doubles()
             .map(x -> Math.sqrt(x * 1_000_000 + 3))
-            .mapToInt(x -> (int)Math.floor(x))
-            .filter(primeTester) // longer way: .filter(x -> primeTester.test(x))
+            .mapToInt(x -> (int)x)
+            .filter(PRIME_TESTER) // longer way: .filter(x -> PRIME_TESTER.test(x))
             .skip(10) // skip the first 10
             .limit(90) // and take the first 90 of the rest
             .map(e -> e * e) // square the numbers
-            .reduce(0, Integer::sum) // and add them up
+            .reduce(0, (state, elem) -> state + elem) // and add them up
         );
         
         // Many other standard classes in Java are retrofitted to generate streams.
         List<Integer> nums = Arrays.asList(16, 15, 22, 9, 7, 82, 17);
         System.out.println("The first prime number in the list is " + nums.stream()
-        .mapToInt(Integer::intValue)
-        .filter(primeTester)
+        .mapToInt(Integer::intValue) // method reference with :: operator
+        .filter(PRIME_TESTER)
         .findFirst() // An OptionalInt, as stream could be empty (this one isn't)
         .orElse(-1) // This is an OptionalInt method, not a Collector
         );
@@ -52,7 +52,7 @@ public class MapReduceDemo {
         nums = Arrays.asList(4, 8, 15); // Let's try the same thing with no primes.
         System.out.println("The first prime number in the list is " + nums.stream()
         .mapToInt(x -> x)
-        .filter(primeTester)
+        .filter(PRIME_TESTER)
         .findFirst()
         .orElse(-1));
         
@@ -64,7 +64,7 @@ public class MapReduceDemo {
         // A huge stream 0, 1, 2, ... , 1,000,000,000. No problem, since Java 8
         // streams are evaluated in a lazy fashion.
         IntStream is = IntStream.rangeClosed(0, 1_000_000_000)
-            .filter(primeTester.negate()); // All predicates have negate() as a default method
+            .filter(PRIME_TESTER.negate()); // Predicates have a default method negate()
             
         // Notice how the stream has been defined, but no computation takes place
         // yet. The stream itself is an object, just sitting there in the memory,
@@ -72,12 +72,14 @@ public class MapReduceDemo {
         // as a result. Attaching some collector to the end of the stream will then
         // actually launch the evaluation by requesting elements from the stream.
         
-        System.out.println("\nThe first 20 nonprimes are " +
-            is.limit(20).boxed().collect(Collectors.toList())
+        System.out.println("\nThe first 20 non-primes are " +
+            is.limit(20)
+                    .boxed()
+                    .collect(Collectors.toList())
         );
         
-        // Infinite stream of random numbers. Again, laziness prevents this computation
-        // from falling into an infinite loop.
+        // Infinite stream of random numbers. Again, lazy evaluation prevents this
+        // computation from falling into an infinite loop.
         System.out.println("Here are some filtered random numbers.");
         Random rng = new Random();
         System.out.println(
@@ -103,7 +105,7 @@ public class MapReduceDemo {
             }
         }
         
-        // Stateful predicates with fields cannot be implement as lambdas. But they are
+        // Stateful predicates with fields cannot be implemented as lambdas. But they are
         // still classes, so we can write them explicitly as such. Also, the result of
         // the predicate can depend on things other than the element, although such a
         // stateful predicate would be nondeterministic if used in a parallel stream.
@@ -125,7 +127,7 @@ public class MapReduceDemo {
             //.parallel() // uncomment this line for some goofy nondeterminism
             .filter(new CountPredicate<>(5))
             .limit(100)
-            .forEach(System.out::println); // method reference with :: operator    
+            .forEach(System.out::println);
         
         // flatMap is a handy stream operator to expand individual elements to many elements.
         System.out.println("Prefix of the \"pyramid series\" generated with flatMap: ");
