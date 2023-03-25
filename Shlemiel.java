aa5727ad938aa60cae314fe979a1c3779c2ccf42
Ilkka Kokkarinen <ilkka.kokkarinen@gmail.com>
2023-03-25 16:30:12 -0400

    Add files via upload

--- Shlemiel.java
+++ Shlemiel.java
@@ -1,20 +1,18 @@
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Stack;
+import java.util.HashMap;
 
 /**
- * Many array problems are easy to solve in {@code O(n^2)} quadratic time with a
- * "Shlemiel" approach, but with some clever thinking, they can be turned into
- * linear time {@code O(n)} single pass algorithms. This class illustrates some
- * techniques to achieve this.
+ * Many array problems are easy to solve in {@code O(n^2)} time with the "Shlemiel"
+ * approach, but with some clever thinking, they can be turned into {@code O(n)} time
+ * single pass algorithms.
  * @author Ilkka Kokkarinen
  */
-
 public class Shlemiel {
     
     /**
      * Given an integer array {@code a}, create the accumulation array {@code b}
-     * whose each element equals the sum of elements in a up to that position.
+     * where each element equals the sum of elements in a up to that position.
      * @param a The original array to accumulate.
      * @return The accumulation array of the original array.
      */
@@ -23,7 +21,7 @@ public class Shlemiel {
         for(int i = 0; i < b.length; i++) {
             int sum = 0;
             for(int j = 0; j <= i; j++) {
-                sum += a[i];
+                sum += a[j];
             }
             b[i] = sum;
         }
@@ -32,12 +30,13 @@ public class Shlemiel {
     
     /**
      * Given an integer array {@code a}, create the accumulation array {@code b}
-     * whose each element equals the sum of elements in a up to that position.
+     * where each element equals the sum of elements in a up to that position.
      * @param a The original array to accumulate.
      * @return The accumulation array of the original array.
      */
     public static int[] accumulate(int[] a) {
         int[] b = new int[a.length];
+        if(b.length == 0) { return b; }
         b[0] = a[0];
         for(int i = 1; i < a.length; i++) {
             b[i] = b[i-1] + a[i];
@@ -46,71 +45,131 @@ public class Shlemiel {
     }
     
     /**
-     * Compute the n:th Fibonacci number using exponential recursion, therefore
-     * computing the same subproblems over and over again.
-     * @param n The position of the Fibonacci number to compute.
-     * @return The n:th Fibonacci number.
+     * Given an integer array {@code a}, find and return the length of its
+     * longest contiguous strictly ascending subarray.
+     * @param a The array to search the ascending subarray in.
+     * @return The length of the longest ascending subarray.
      */
-    public static int fibonacciRec(int n) {
-        if(n < 2) { return 1; }
-        else { return fibonacciRec(n-1) + fibonacciRec(n-2); }
-    }
-    
-    /**
-     * Compute the n:th Fibonacci number using a dynamic programming loop that
-     * fills in the array of solutions to subproblems in some order that guarantees
-     * that whenever the loop comes to given position, the elements that the recursion
-     * would require to have already been computed. Recursive calls can now be turned
-     * into simple constant-time array lookups.
-     * @param n The position of the Fibonacci number to compute.
-     * @return The n:th Fibonacci number.
-     */
-    public static int fibonacciDyn(int n) {
-        int[] fib = new int[n+1];
-        fib[0] = fib[1] = 1;
-        for(int i = 2; i <= n; i++) {
-            fib[i] = fib[i-1] + fib[i-2];
+    public static int longestAscendingSubarrayShlemiel(int[] a) {
+        if(a.length == 0) { return 0; }
+        int max = 1;        
+        for(int i = 0; i < a.length - 1; i++) {
+            int j = i + 1;
+            int len = 1;
+            while(j < a.length && a[j] > a[j-1]) { j++; len++; }
+            if(len > max) { max = len; }
         }
-        return fib[n];
+        return max;
     }
     
     /**
-     * Checks whether there exists some index {@code i} into the array {@code a} so that
-     * the sum of elements in the left subarray from beginning to {@code i} equals the
-     * sum of elements in the right subarray from index {@code i + 1} to the end. 
-     * @param a The array to determine if such splitting index exists.
-     * @return {@code true} if at least one such index exists, {@code false} otherwise.
+     * Given an integer array {@code a}, find and return the length of its
+     * longest contiguous strictly ascending subarray.
+     * @param a The array to search the ascending subarray in.
+     * @return The length of the longest ascending subarray.
      */
-    public static boolean canBalanceShlemiel(int[] a) {
-        for(int i = 0; i < a.length; i++) { // possible splitting points
-            int leftSum = 0, rightSum = 0;
-            for(int j = 0; j <= i; j++) { leftSum += a[j]; }
-            for(int j = i+1; j < a.length; j++) { rightSum += a[j]; }
-            if(leftSum == rightSum) { return true; }
+    public static int longestAscendingSubarray(int[] a) {
+        if(a.length == 0) { return 0; }
+        int curr = 1; // The length of the current ascension.
+        int max = 1; // The longest ascension that we have seen so far.
+        for(int i = 1; i < a.length; i++) { // Start from second element
+            if(a[i] > a[i-1]) {
+                curr++;
+                if(curr > max) { max = curr; }
+            }
+            else {
+                curr = 1;
+            }
         }
-        return false;
+        return max;
     }
-    
+        
     /**
-     * Checks whether there exists some index {@code i} into the array {@code a} so that
-     * the sum of elements in the left subarray from beginning to {@code i} equals the
-     * sum of elements in the right subarray from index {@code i + 1} to the end. 
-     * @param a The array to determine if such splitting index exists.
-     * @return {@code true} if at least one such index exists, {@code false} otherwise.
+     * Given an integer array {@code a} guaranteed to be sorted in ascending order,
+     * determine whether it contains two elements whose sum equals goal value {@code x}.
+     * @param a The array to search the two elements in.
+     * @return Whether two such elements exist.
      */
-    public static boolean canBalance(int[] a) {
-        int leftSum = 0, rightSum = 0;
-        for(int e: a) { rightSum += e; }
-        for (int j : a) {
-            leftSum += j; // O(1) update of leftSum and rightSum, instead of O(n)
-            rightSum -= j;
-            if (leftSum == rightSum) {
-                return true;
+    public static boolean twoSummingElementsShlemiel(int[] a, int x) {
+        for(int i = 0; i < a.length; i++) {
+            for(int j = i + 1; j < a.length && a[i] + a[j] <= x; j++) {
+                if(a[i] + a[j] == x) { return true; }
             }
         }
         return false;
     }
     
+    /**
+     * Given an integer array {@code a} guaranteed to be sorted in ascending order,
+     * determine whether it contains two elements whose sum equals goal value {@code x}.
+     * @param a The array to search the two elements in.
+     * @return Whether two such elements exist.
+     */
+    public static boolean twoSummingElements(int[] a, int x) {
+        int i = 0, j = a.length - 1;
+        // Invariant: If such a pair exists in array, such pair exists between indices i and j, inclusive.
+        while(i < j) {
+            int sum = a[i] + a[j];
+            if(sum == x) { return true; }
+            else if(sum < x) { i++; } // smallest element is too small to work, advance left
+            else { j--; } // largest element too large to work, advance right            
+        }
+        return false;        
+    }
+    
+    // Puzzle for thought: suppose the previous task was to find three summing elements
+    // that add up to x. Shlemiel would solve this in O(n^3) time. Can you solve this in
+    // O(n^2) time? How about O(n) time?
+    
+    /**
+     * Evaluate the polynomial at the given point x.
+     * @param coefficients The coefficients of the polynomial.
+     * @param x The point in which to evaluate the polynomial.
+     * @return The value of the polynomial at point {@code x}.
+     */
+    public static double evaluatePolynomialShlemiel(double[] coefficients, double x) {
+        double sum = 0;
+        for(int i = 0; i < coefficients.length; i++) {
+            double pow = 1;
+            // Evaluate each term from scratch in the linear inner loop.
+            for(int j = 0; j < i; j++) { pow = pow * x; }
+            sum += coefficients[i] * pow;
+        }
+        return sum;
+    }
+    
+    /**
+     * Evaluate the polynomial at the given point x.
+     * @param coefficients The coefficients of the polynomial.
+     * @param x The point in which to evaluate the polynomial.
+     * @return The value of the polynomial at point {@code x}.
+     */
+    public static double evaluatePolynomialLinear(double[] coefficients, double x) {
+        double sum = 0, pow = 1;
+        // Two multiplications and one add per coefficient.
+        for(double coeff: coefficients) {
+            sum += coeff * pow; // Add the current power to the result...
+            pow = pow * x; // and use it to compute the next power
+        }
+        return sum;
+    }
+    
+    /**
+     * Evaluate the polynomial at the given point x.
+     * @param coefficients The coefficients of the polynomial.
+     * @param x The point in which to evaluate the polynomial.
+     * @return The value of the polynomial at point {@code x}.
+     */
+    public static double evaluatePolynomialHorner(double[] coefficients, double x) {
+        double sum = 0;
+        // One multiplication and one add per coefficient.
+        for(int i = coefficients.length - 1; i >= 0; i--) {
+            sum = sum * x + coefficients[i];
+        }
+        // This is also more numerically stable when using floating point.
+        return sum;
+    }
+
     /**
      * Given an {@code n}-element integer array, determine whether it contains
      * each number from 1 to {@code n} exactly once.
@@ -138,7 +197,7 @@ public class Shlemiel {
      */
     public static boolean containsAllNumbersSorting(int[] a, int n) {
         // We can do better by sorting the array first.
-        Arrays.sort(a); // O(n log n) stage dominates
+        Arrays.sort(a); // O(n log n) stage dominates asymptotic running time
         for(int i = 0; i < n; i++) { // O(n)
             if(a[i] != i+1) { return false; }
         }
@@ -154,44 +213,99 @@ public class Shlemiel {
      */
     public static boolean containsAllNumbersLinear(int[] a, int n) {
         // For a linear time solution, trade memory for time: use a boolean
-        // array to remember which values we have already seen. Allocate
-        // one more element than needed to save two subtractions per round.
+        // array to remember which values we have already seen. (Also, allocate
+        // one more element than needed to eliminate need for integer subtract.)
         boolean[] alreadySeen = new boolean[n + 1];
-        for(int e: a) {
+        for(int e : a) {
             if(e < 1 || e > n || alreadySeen[e]) { return false; }
             alreadySeen[e] = true;
         }
         return true;
-    }
+    }        
     
     /**
-     * Remove all strings from the given arraylist of strings whose length is shorter than len.
-     * @param a The arraylist of strings to process.
+     * Remove all strings from the given arraylist of strings whose length is shorter than {@code len}.
+     * @param strings The arraylist of strings to process.
      * @param len The threshold length for a string to remain in the list.
      */
-    public static void removeShortStringsShlemiel(ArrayList<String> a, int len) {
+    public static void removeShortStringsShlemiel(ArrayList<String> strings, int len) {
         int i = 0;
-        while(i < a.size()) {
-            if(a.get(i).length() < len) {
-                a.remove(i); // removing element from middle takes linear time
-            }
-            else { i++; }
+        while(i < strings.size()) {
+            if(strings.get(i).length() < len) { strings.remove(i); } // remove from middle is O(n)
+            else { i++; } // advance only if skipping the current element
         }
-        // Total worst case running time is O(n) * O(n) = O(n^2)
+        // Total worst case running time is O(n) * O(n) = O(n ^ 2)
     }
     
     /**
-     * Remove all strings from the given arraylist of strings whose length is shorter than len.
-     * @param a The arraylist of strings to process.
+     * Remove all strings from the given arraylist of strings whose length is shorter than {@code len}.
+     * @param strings The arraylist of strings to process.
      * @param len The threshold length for a string to remain in the list.
      */
-    public static void removeShortStrings(ArrayList<String> a, int len) {
-        ArrayList<String> tmp = new ArrayList<>();
-        for(String e : a) { // total of O(n) over n rounds
-            if(e.length() >= len) { tmp.add(e); } // O(1) amortized
+    public static void removeShortStrings(ArrayList<String> strings, int len) {
+        ArrayList<String> tmp = new ArrayList<String>();
+        for(String e : strings) { // total of O(n) over n rounds
+            if(e.length() >= len) { tmp.add(e); } // O(1) amortized add to end
         }
-        a.clear(); // O(n) (references are set to null to let garbage collection proceed)
-        a.addAll(tmp); // O(n)
-        // Total worst case running time is O(n) + O(n) + O(n) = O(n)
+        strings.clear(); // O(n) (references are set to null to enable garbage collection)
+        strings.addAll(tmp); // O(n)
+        // Total worst case running time is 3 * O(n) = O(n)
+    }
+
+    /**
+     * Determine whether the array {@code items} contains a strict majority element.
+     * @param items The array from which the look for the majority element.
+     * @return Whether there exists a majority element.
+     */
+    public static boolean hasMajorityShlemiel(int[] items) {
+        for(int e: items) {
+            int edge = 0;
+            for(int ee: items) {
+                edge += (ee == e? +1 : -1);
+            }
+            if(edge > 0) { return true; }
+        }
+        return false;
+    }
+
+    /**
+     * Determine whether the array {@code items} contains a strict majority element.
+     * @param items The array from which the look for the majority element.
+     * @return Whether there exists a majority element.
+     */
+    public static boolean hasMajorityHashMap(int[] items) {
+        HashMap<Integer, Integer> counters = new HashMap<>();
+        for(int e: items) {
+            counters.put(e, counters.getOrDefault(e, 0) + 1);
+        }
+        for(int e: counters.keySet()) {
+            if(counters.get(e) * 2 > items.length) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Determine whether the array {@code items} contains a strict majority element.
+     * @param items The array from which the look for the majority element.
+     * @return Whether there exists a majority element.
+     */
+    public static boolean hasMajorityLinear(int[] items) {
+        int count = 0, curr = 0;
+        for(int e: items) {
+            if(count == 0) {
+                curr = e; count = 1;
+            }
+            else {
+                count += (curr == e ? +1 : -1);
+            }
+        }
+        // At this point, if there is a majority element, curr is it. So let's check.
+        int edge = 0;
+        for(int e: items) {
+            edge += (curr == e ? +1 : -1);
+        }
+        return edge > 0;
     }
 }
